37d8b1af2269bedbdda53336c1cf0e3aa7ded4ab
Jomel Cadiente <cadientejomel0@gmail.com>
2022-10-19 20:28:55 +0800

    added payments model

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
+import logging
 from django.contrib import admin
+from django.apps import apps
 
+logger = logging.getLogger("main")
+
+app = apps.get_app_config("payments")
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRigstered as err:
+        logger.info("App already registered")
 # Register your models here.
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,45 @@
+from decimal import Decimal, getcontext
+from django.utils.timezone import datetime
 from django.db import models
+from users.models import Tenant
+from buildings.models import Room, TenantRoom
+from core.models import BaseModel
 
-# Create your models here.
+from .utils import generate_transation_number
+
+getcontext().prec = 2
+
+class Payment(BaseModel):
+    class STATUS(models.TextChoices):
+        UNPAID = "up", "Unpaid"
+        PAID = "pd", "Paid"
+        PARTIAL = "p", "Partial"
+
+    class TYPE(models.TextChoices):
+        RENT = "R", "Rent",
+        MISCELLANEOUS = "Misc", "Miscellaneous"
+
+    to = models.DateField(blank=True, null=True)
+    until = models.DateField(blank=True, null=True)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS.choices, default=STATUS.UNPAID)
+    type = models.CharField(max_length=15, choices=TYPE.choices, default=TYPE.RENT)
+    tenant = models.ForeignKey(TenantRoom, on_delete=models.CASCADE, related_name="tenant_payments")
+    due_date = models.DateField()
+
+    def __str__(self):
+        return f"Payment issued to {self.tenant.tenant.get_full_name}"
+
+class Transaction(BaseModel):
+    transaction_no = models.CharField(max_length=15, unique=True, editable=False, default=generate_transation_number)
+    payment_received = models.DecimalField(max_digits=8, decimal_places=2)
+    date_received = models.DateField()
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="payment_transactions")
+    
+    @property
+    def remaining_balance(self) -> Decimal:
+        return Decimal(self.payment.amount) - Decimal(self.payment_received)
+        
+    def __str__(self):
+        return f"{self.transaction_no}"
+
